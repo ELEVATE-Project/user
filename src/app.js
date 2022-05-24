ed907b8caf7690b792faff9dcd5afeec8010de61
joffinjoy <35325730+joffinjoy@users.noreply.github.com>
2022-05-24 10:27:21 +0530

    Added Src Directory & Prettier Pre-Hook

---
+++ src/app.js
@@ -0,0 +1,70 @@
+/**
+ * name : app.js
+ * author : Aman Kumar Gupta
+ * Date : 29-Sep-2021
+ * Description : Start file of a user service
+ */
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config({ path: './.env' })
+require('./configs')
+
+let environmentData = require('./envVariables')()
+
+if (!environmentData.success) {
+	console.log('Server could not start . Not all environment variable is provided')
+	process.exit()
+}
+
+const app = express()
+
+// Health check
+require('./health-checks')(app)
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50MB' }))
+app.use(bodyParser.json({ limit: '50MB' }))
+
+app.use(express.static('public'))
+
+/* Logs request info if environment is not development*/
+if (process.env.ENABLE_LOG === 'true') {
+	app.all('*', (req, res, next) => {
+		console.log('***Notification Service Logs Starts Here***')
+		console.log('%s %s on %s from ', req.method, req.url, new Date(), req.headers['user-agent'])
+		console.log('Request Headers: ', req.headers)
+		console.log('Request Body: ', req.body)
+		console.log('Request Files: ', req.files)
+		console.log('***Notification Service Logs Ends Here***')
+		next()
+	})
+}
+
+/* Registered routes here */
+require('./routes')(app)
+
+// Server listens to given port
+app.listen(process.env.APPLICATION_PORT, 'localhost', (res, err) => {
+	if (err) {
+		onError(err)
+	}
+	console.log('Environment: ' + process.env.APPLICATION_ENV)
+	console.log('Application is running on the port:' + process.env.APPLICATION_PORT)
+})
+
+// Handles specific listen errors with friendly messages
+function onError(error) {
+	switch (error.code) {
+		case 'EACCES':
+			console.log(process.env.APPLICATION_PORT + ' requires elevated privileges')
+			process.exit(1)
+		case 'EADDRINUSE':
+			console.log(process.env.APPLICATION_PORT + ' is already in use')
+			process.exit(1)
+		default:
+			throw error
+	}
+}
