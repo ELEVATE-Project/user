ccc35ce80529fdc9e0eaabd9c7f0b2d95a37738a
rockySagar <rakesh.doddmane@gmail.com>
2022-06-01 16:44:43 +0530

    test case folder added

---
+++ src/test/utils.js
@@ -0,0 +1,12 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const utils = require('../generics/utils')
+
+describe('Utils ', function () {
+	it('should check composeEmailBody', async function () {
+		let emailBody = 'Hi {name}, welcome to elevate'
+		const emaiCompose = utils.composeEmailBody(emailBody, { name: 'user' })
+		expect(emaiCompose).to.equal('Hi user, welcome to elevate')
+	})
+})
