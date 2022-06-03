07fa959fa374be1f5c5ecc6af890d0086ccf4ec6
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2022-06-03 12:21:51 +0530

    cron jobs splitted into different APIs

--- src/controllers/v1/notifications.js
+++ src/controllers/v1/notifications.js
@@ -11,16 +11,50 @@ const httpStatusCode = require('../../generics/http-status')
 
 module.exports = class Notifications {
 	/**
-	 * Notification email cron job.
+	 * Notification email cron job before 15min.
 	 * @method
-	 * @name emailCronJob
+	 * @name emailCronJobBeforeFifteenMin
 	 * @returns {JSON} - Send email notification.
 	 */
 
-	async emailCronJob() {
+	async emailCronJobBeforeFifteenMin() {
 		try {
 			notificationsHelper.sendNotificationBefore15mins()
+			return {
+				statusCode: httpStatusCode.ok,
+			}
+		} catch (error) {
+			return error
+		}
+	}
+
+	/**
+	 * Notification email cron job before 24hrs.
+	 * @method
+	 * @name emailCronJobBeforeOneDay
+	 * @returns {JSON} - Send email notification.
+	 */
+
+	async emailCronJobBeforeOneDay() {
+		try {
 			notificationsHelper.sendNotificationBefore24Hour()
+			return {
+				statusCode: httpStatusCode.ok,
+			}
+		} catch (error) {
+			return error
+		}
+	}
+
+	/**
+	 * Notification email cron job before 1hr.
+	 * @method
+	 * @name emailCronJobBeforeOneHour
+	 * @returns {JSON} - Send email notification.
+	 */
+
+	async emailCronJobBeforeOneHour() {
+		try {
 			notificationsHelper.sendNotificationBefore1Hour()
 			return {
 				statusCode: httpStatusCode.ok,
