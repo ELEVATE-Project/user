2c1a783301273bfafebe8d332882a58a62020de6
Nevil Mathew <nevil@tunerlabs.com>
2022-07-05 18:53:30 +0530

    validation fix

--- src/validators/v1/profile.js
+++ src/validators/v1/profile.js
@@ -53,7 +53,7 @@ module.exports = {
 
 	details: (req) => {},
 
-	shareMentorProfile: (req) => {
+	share: (req) => {
 		req.checkParams('id').notEmpty().withMessage('id param is empty').isMongoId().withMessage('id is invalid')
 	},
 }
