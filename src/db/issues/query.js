9f61c1a622d88468ace6b97fd730460d07807d0f
Nevil Mathew <nevil@tunerlabs.com>
2022-07-15 17:13:32 +0530

    added test for report apis

--- src/db/issues/query.js
+++ src/db/issues/query.js
@@ -4,8 +4,8 @@ module.exports = class issueData {
 	static create(data) {
 		return new Promise(async (resolve, reject) => {
 			try {
-				await new Issues(data).save()
-				resolve(true)
+				let response = await new Issues(data).save()
+				resolve(response)
 			} catch (error) {
 				reject(error)
 			}
---
+++ src/test/help.spec.js
@@ -0,0 +1,86 @@
+describe('Help api test', () => {
+	const mongoose = require('mongoose')
+
+	async function loadMongo() {
+		let db = await mongoose.connect(global.__MONGO_URI__ + global.mongoDBName, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		global.db = db
+		return
+	}
+
+	describe('Report api test', () => {
+		let help
+		let notificationTemplateData
+		let issueData
+		let kafkaCommunication
+		const OLD_ENV = process.env
+
+		beforeAll(async () => {
+			await loadMongo()
+			help = require('@services/helper/help')
+			notificationTemplateData = require('@db/notification-template/query')
+			kafkaCommunication = require('@generics/kafka-communication')
+			issueData = require('@db/issues/query')
+
+			//jest.resetModules() //clears the cache
+			process.env = { ...OLD_ENV } // Make a copy
+			return
+		})
+
+		test('Should return mentors sharable profile link', async () => {
+			process.env.ENABLE_EMAIL_FOR_REPORTED_ISSUE = true
+
+			const expectedResult = {
+				responseCode: 'OK',
+				message: 'Your issue was successfully reported.',
+				result: [],
+			}
+			const decodedToken = {
+				_id: '62d00b7c082b1ebc88a2a095',
+				email: 'nevil.mentor@tunerlabs.com',
+				name: 'Nevil',
+				isAMentor: true,
+			}
+			const bodyData = {
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			}
+
+			const notificationTemplateDataResponse = {
+				subject: 'MentorED - A new issue has been reported.',
+				body: '<div><p>Hi Team,</p>{role} {name} is facing issue in <b>{description}</b> in 2.1 version of IMentorED.</div>',
+			}
+
+			let createdIssue = await issueData.create({
+				description: 'Test description',
+			})
+
+			expect(createdIssue.description).toBe('Test description')
+
+			const kafkaResponse = { notificationtopic: { 0: 25 } }
+
+			const templateData = jest.spyOn(notificationTemplateData, 'findOneEmailTemplate')
+			templateData.mockResolvedValueOnce(notificationTemplateDataResponse)
+
+			const kafkaData = jest.spyOn(kafkaCommunication, 'pushEmailToKafka')
+			kafkaData.mockResolvedValueOnce(kafkaResponse)
+
+			const actual = await help.report(bodyData, decodedToken)
+			expect(actual.responseCode).toEqual(expectedResult.responseCode)
+			expect(actual.result).toEqual(expectedResult.result)
+		})
+		afterAll(async () => {
+			try {
+				mongoose.connection.close()
+				process.env = OLD_ENV // Restore old environment
+			} catch (error) {
+				console.log(`
+            You did something wrong
+            ${error}
+          `)
+				throw error
+			}
+		})
+	})
+})
