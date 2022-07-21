5918689486884ac6c9a549766a76b2056539618e
ankitshahu <ankit@matchlog.delivery>
2022-07-21 16:16:47 +0530

    moved files to cache js

--- src/configs/index.js
+++ src/configs/index.js
@@ -9,7 +9,4 @@ require('./mongodb')()
 
 require('./kafka')()
 
-const { RedisConfig, InternalCache, RedisHelper } = require('elevate-node-cache')
-RedisConfig.config(process.env.REDIS_HOST)
-InternalCache.init(process.env.INTERNAL_CACHE_EXP_TIME)
-RedisHelper.init(process.env.REDIS_CACHE_EXP_TIME)
+require('./cache')()
