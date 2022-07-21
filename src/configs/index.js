edc60a78062e535e58c31e101a242e07d33cce84
ankitshahu <ankit@matchlog.delivery>
2022-07-21 16:19:01 +0530

    added cache js file

--- src/configs/index.js
+++ src/configs/index.js
@@ -8,11 +8,7 @@
 //Dependencies
 require('./mongodb')()
 require('./kafka')()
-
-const { RedisConfig, InternalCache, RedisHelper } = require('elevate-node-cache')
-RedisConfig.config(process.env.REDIS_HOST)
-InternalCache.init(process.env.INTERNAL_CACHE_EXP_TIME)
-RedisHelper.init(process.env.REDIS_CACHE_EXP_TIME)
+require('./cache')()
 const path = require('path')
 
 global.PROJECT_ROOT_DIRECTORY = path.join(__dirname, '..')
