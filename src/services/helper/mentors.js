deb97dbc3fc1ef3553bdd2bc2fe2e4a493bf1bef
ankitshahu <ankit@matchlog.delivery>
2022-07-22 13:37:40 +0530

    Fixed session hosted

--- src/services/helper/mentors.js
+++ src/services/helper/mentors.js
@@ -143,6 +143,7 @@ module.exports = class MentorsHelper {
 				userId: ObjectId(userId),
 				status: 'completed',
 				deleted: false,
+				isStarted: true,
 			}
 
 			totalsessionHosted = await sessionsData.countSessions(filters)
