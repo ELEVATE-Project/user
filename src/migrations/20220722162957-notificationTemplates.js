c09942d5e38ffc86890fc0a116f70d5d0b246318
Nevil <nevil@tunerlabs.com>
2022-08-29 18:56:39 +0530

    added email migrations

--- src/migrations/20220722162957-notificationTemplates.js
+++ src/migrations/20220722162957-notificationTemplates.js
@@ -15,6 +15,12 @@ let emailTemplates = [
 		body: '<div><p>Dear {name},</p> Your OTP to complete the registration process is {otp}. Please enter the OTP to complete the registration. For your security, please do not share this OTP with anyone.</div>',
 	},
 	{
+		code: 'user_issue_reported',
+		subject: 'Support request for MentorED',
+		body: '<div><p>Hi Team,</p>{role} {name} is facing issue in <b>{description}</b>-{userEmailId} in 2.1 version of MentorED.</div>',
+	},
+
+	{
 		code: 'email_footer',
 		body: "</div><div style='margin-top:20px;text-align:center;'><div>Regards,</div><div>Team MentorED</div><div style='margin-top:20px;color:#b13e33;text-align:center'><div>Note: Do not reply to this email. This email is sent from an unattended mailbox. Replies will not be read.</div><div>For any queries, please feel free to reach out to us at support@shikshalokam.org</div></div></div></div>",
 	},
