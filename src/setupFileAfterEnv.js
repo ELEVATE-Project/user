37e399dee3929407a2d2752e09c4ea60b9e12f9f
Nevil <nevil@tunerlabs.com>
2022-10-11 17:04:14 +0530

    running test outside docker

--- src/setupFileAfterEnv.js
+++ src/setupFileAfterEnv.js
@@ -5,16 +5,10 @@ const { matchers } = require('jest-json-schema')
 expect.extend(matchers)
 
 //Connect to database
-let db
-if (process.env.inContainer) {
-	db = mongoose.createConnection(process.env.MONGODB_URL, {
-		useNewUrlParser: true,
-	})
-} else {
-	db = mongoose.createConnection('mongodb://127.0.0.1:27017/elevate-mentoring', {
-		useNewUrlParser: true,
-	})
-}
+
+const db = mongoose.createConnection('mongodb://127.0.0.1:27017/elevate-mentoring', {
+	useNewUrlParser: true,
+})
 
 db.on('error', function () {
 	console.log('Database connection error:')
