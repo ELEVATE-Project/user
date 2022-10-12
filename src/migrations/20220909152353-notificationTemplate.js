d95d61a78c65530e9f1d091d75dd2a6eb18e6e35
Nevil <nevil@tunerlabs.com>
2022-10-12 13:58:52 +0530

    updated email template migration

--- src/migrations/20220909152353-notificationTemplate.js
+++ src/migrations/20220909152353-notificationTemplate.js
@@ -4,15 +4,6 @@ let emailTemplates = [
 		subject: 'Support request for MentorED',
 		body: '<div><p>Hi Team,</p>{role} {name} is facing issue in <b>{description}</b>-{userEmailId} in 2.1 version of MentorED.<p>Kindly look into it.</p><p>Device name:{deviceName}</p><p>Android Version:{androidVersion}</p></div>',
 	},
-
-	{
-		code: 'email_footer',
-		body: "</div><div style='margin-top:20px;text-align:center;'><div>Regards,</div><div>Team MentorED</div><div style='margin-top:20px;color:#b13e33;text-align:center'><div>Note: Do not reply to this email. This email is sent from an unattended mailbox. Replies will not be read.</div><div>For any queries, please feel free to reach out to us at support@shikshalokam.org</div></div></div></div>",
-	},
-	{
-		code: 'email_header',
-		body: "<div style='margin:auto;width:100%;max-width:650px;'><p style='text-align:center'><img class='imgPath' style='width:35%' alt='MentorED' src='https://mentoring-dev-storage.s3.ap-south-1.amazonaws.com/email/image/emailLogo.png'></p><div style='text-align:center'>",
-	},
 ]
 var moment = require('moment')
 
