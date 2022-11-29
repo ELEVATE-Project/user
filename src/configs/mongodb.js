96888b7a2456b974f14b8af3a3cfb198de4f4a7b
rakesh kumar <rakeshkumar@rakeshs-MacBook-Air.local>
2022-11-29 17:59:42 +0530

    mongo replica set changes

--- src/configs/mongodb.js
+++ src/configs/mongodb.js
@@ -10,11 +10,18 @@ const mongoose = require('mongoose')
 const mongoose_autopopulate = require('mongoose-autopopulate')
 const mongoose_timestamp = require('mongoose-timestamp')
 
-
 module.exports = function () {
 	// Added to remove depreciation warnings from logs.
 
-	const db = mongoose.createConnection(process.env.MONGODB_URL, {
+	let parameters
+	if (process.env.REPLICA_SET_NAME) {
+		parameters = '?replicaSet=' + process.env.REPLICA_SET_NAME
+	}
+	if (process.env.REPLICA_SET_NAME && process.env.REPLICA_SET_READ_PREFERENCE) {
+		parameters = parameters + '&readPreference=' + process.env.REPLICA_SET_READ_PREFERENCE
+	}
+
+	const db = mongoose.createConnection(process.env.MONGODB_URL + parameters, {
 		useNewUrlParser: true,
 	})
 
