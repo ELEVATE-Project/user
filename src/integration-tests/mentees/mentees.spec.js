8f20b98081614f98a431b1eee5f5539f737ef6b4
Nevil <nevil@tunerlabs.com>
2023-04-24 14:42:49 +0530

    update /joinSession test case

--- src/integration-tests/mentees/mentees.spec.js
+++ src/integration-tests/mentees/mentees.spec.js
@@ -17,7 +17,7 @@ describe('mentoring/v1/mentees', function () {
 		let sessionId = await sessionsData.insertSession((now = true), (sessionStatus = 'live'))
 		await sessionsData.insertSessionAttendee(sessionId)
 		let res = await request.post('/mentoring/v1/mentees/joinSession/' + sessionId)
-		//console.log(res.body)
+		console.log(res.body)
 		expect(res.statusCode).toBe(200)
 		expect(res.body).toMatchSchema(schema.joinSessionSchema)
 	})
