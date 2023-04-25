4a7d9a30364d26bc47300e518c9c07e2960b945d
rakeshSgr <rakeshkumar@PW110.local>
2023-04-25 18:19:16 +0530

    healthcheckFix

--- src/health-checks/kafka.js
+++ src/health-checks/kafka.js
@@ -8,25 +8,23 @@
 // Dependencies
 const { Kafka } = require('kafkajs')
 
-function health_check() {
-	return new Promise(async (resolve, reject) => {
-		const kafkaIps = process.env.KAFKA_URL.split(',')
-		const KafkaClient = new Kafka({
-			clientId: 'mentoring',
-			brokers: kafkaIps,
-		})
+async function health_check() {
+	const kafkaIps = process.env.KAFKA_URL.split(',')
+	const KafkaClient = new Kafka({
+		clientId: 'mentoring',
+		brokers: kafkaIps,
+	})
 
-		const producer = KafkaClient.producer()
-		await producer.connect()
+	const producer = KafkaClient.producer()
+	await producer.connect()
 
-		producer.on('producer.connect', () => {
-			console.log(`KafkaProvider: connected`)
-			return resolve(true)
-		})
-		producer.on('producer.disconnect', () => {
-			console.log(`KafkaProvider: could not connect`)
-			return resolve(false)
-		})
+	producer.on('producer.connect', () => {
+		console.log(`KafkaProvider: connected`)
+		return true
+	})
+	producer.on('producer.disconnect', () => {
+		console.log(`KafkaProvider: could not connect`)
+		return false
 	})
 }
 
