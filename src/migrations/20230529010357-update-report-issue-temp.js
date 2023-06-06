8a2f907b712370593a8e6bfc553f32b4a3ae2902
Nevil <nevil@tunerlabs.com>
2023-06-06 16:38:26 +0530

    updated report issue template

--- src/migrations/20230529010357-update-report-issue-temp.js
+++ src/migrations/20230529010357-update-report-issue-temp.js
@@ -3,7 +3,7 @@ var moment = require('moment')
 let emailTemplate = {
 	code: 'user_issue_reported',
 	subject: 'Support request for MentorED',
-	body: '<div><p>Hi Team,</p><p>{role} {name} is facing an issue in <b>{description}</b>-{userEmailId} in 2.1 version of MentorED.</p><p>Kindly look into it.</p><div style="background-color: #f5f5f5; padding: 10px; margin-top: 10px;"><p><b>Meta Information:</b></p><ul style="list-style-type: none; padding: 0;">{metaItems}</ul></div></div>',
+	body: '<div><p>Hi Team,</p><p>{role} {name}, is facing an issue in <b>{description}</b> -{userEmailId}.</p><p>Kindly look into it.</p><div style="background-color: #f5f5f5; padding: 10px; margin-top: 10px;"><p><b>Meta Information:</b></p><ul style="list-style-type: none; padding: 0;">{metaItems}</ul></div></div>',
 	status: 'active',
 	deleted: false,
 	updatedAt: moment().format(),
