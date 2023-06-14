ab32dfbb534f073e5af321bf499ff1bb2462e55f
joffinjoy <joffinjoy@gmail.com>
2023-06-14 08:52:45 +0530

    Standardized Model Definitions

--- src/database/models/sessionEnrollments.js
+++ src/database/models/sessionEnrollments.js
@@ -1,20 +1,29 @@
 module.exports = (sequelize, DataTypes) => {
-	const SessionEnrollments = sequelize.define('session_enrollments', {
-		id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true,
+	const SessionEnrollment = sequelize.define(
+		'SessionEnrollment',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			mentee_id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			session_id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
 		},
-		mentee_id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-		},
-		session_id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-		},
-	})
-
-	return SessionEnrollments
+		{
+			sequelize,
+			modelName: 'SessionEnrollment',
+			tableName: 'session_enrollments',
+			freezeTableName: true,
+			paranoid: true,
+		}
+	)
+	return SessionEnrollment
 }
