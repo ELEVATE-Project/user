c6218108e08d092c794b6a1152e4c7b6c24ec23e
Nevil <nevil@tunerlabs.com>
2023-10-06 09:46:24 +0530

    fixed import issue

--- src/services/helper/org-admin.js
+++ src/services/helper/org-admin.js
@@ -1,124 +1,123 @@
 // Dependenices
 const common = require('@constants/common')
-const mentorExtensionQueries = require('../../database/queries/mentorextension')
+const mentorQueries = require('../../database/queries/mentorextension')
 const menteeQueries = require('../../database/queries/userextension')
-
+const httpStatusCode = require('@generics/http-status')
 
 module.exports = class OrgAdminService {
 	/**
-     * @description 					- Change user's role based on the current role.
-     * @method
-     * @name 							- roleChange
-     * @param {Object} bodyData 		- The request body containing user data.
-     * @returns {Promise<Object>} 		- A Promise that resolves to a response object.
-     */
+	 * @description 					- Change user's role based on the current role.
+	 * @method
+	 * @name 							- roleChange
+	 * @param {Object} bodyData 		- The request body containing user data.
+	 * @returns {Promise<Object>} 		- A Promise that resolves to a response object.
+	 */
 
 	static async roleChange(bodyData) {
 		try {
 			if (bodyData.current_roles[0] === common.MENTOR_ROLE) {
-                return await this.changeRoleToMentee(bodyData);
-            } else if (bodyData.current_roles[0] === common.MENTEE_ROLE) {
-                return await this.changeRoleToMentor(bodyData);
-            }
-				
+				return await this.changeRoleToMentee(bodyData)
+			} else if (bodyData.current_roles[0] === common.MENTEE_ROLE) {
+				return await this.changeRoleToMentor(bodyData)
+			}
 		} catch (error) {
 			throw error
 		}
 	}
 
 	/**
-     * @description 				- Change user's role to Mentee.
-     * @method
-     * @name 						- changeRoleToMentee
-     * @param {Object} bodyData 	- The request body.
-     * @returns {Object} 			- A Promise that resolves to a response object.
-     */
+	 * @description 				- Change user's role to Mentee.
+	 * @method
+	 * @name 						- changeRoleToMentee
+	 * @param {Object} bodyData 	- The request body.
+	 * @returns {Object} 			- A Promise that resolves to a response object.
+	 */
 	static async changeRoleToMentee(bodyData) {
 		try {
 			// Check current role based on that swap data
 			// If current role is mentor validate data from mentor_extenion table
-			const mentorDetails = await mentorExtensionQueries.getMentorExtension(bodyData.user_id);
+			const mentorDetails = await mentorQueries.getMentorExtension(bodyData.user_id)
 
 			// If such mentor return error
-            if (!mentorDetails) {
-                return common.failureResponse({
-                    message: 'MENTOR_EXTENSION_NOT_FOUND',
-                    statusCode: httpStatusCode.bad_request,
-                    responseCode: 'CLIENT_ERROR',
-                });
-            }
+			if (!mentorDetails) {
+				return common.failureResponse({
+					message: 'MENTOR_EXTENSION_NOT_FOUND',
+					statusCode: httpStatusCode.bad_request,
+					responseCode: 'CLIENT_ERROR',
+				})
+			}
 
 			// Add fetched mentor details to user_extension table
-            const menteeCreationData = await menteeQueries.createMenteeExtension(mentorDetails);
-            if (!menteeCreationData) {
-                return common.failureResponse({
-                    message: 'MENTEE_EXTENSION_CREATION_FAILED',
-                    statusCode: httpStatusCode.bad_request,
-                    responseCode: 'CLIENT_ERROR',
-                });
-            }
+			const menteeCreationData = await menteeQueries.createMenteeExtension(mentorDetails)
+			if (!menteeCreationData) {
+				return common.failureResponse({
+					message: 'MENTEE_EXTENSION_CREATION_FAILED',
+					statusCode: httpStatusCode.bad_request,
+					responseCode: 'CLIENT_ERROR',
+				})
+			}
 
-			// Delete mentor Extension 
-            await mentorQueries.deleteMentorExtension(bodyData.user_id);
+			// Delete mentor Extension
+			await mentorQueries.deleteMentorExtension(bodyData.user_id)
 
-            return common.successResponse({
-                statusCode: httpStatusCode.ok,
-                message: 'USER_ROLE_UPDATED',
-                result: {
-                    user_id: menteeCreationData.user_id,
-                    roles: menteeCreationData.roles
-                },
-            });
+			return common.successResponse({
+				statusCode: httpStatusCode.ok,
+				message: 'USER_ROLE_UPDATED',
+				result: {
+					user_id: menteeCreationData.user_id,
+					roles: menteeCreationData.roles,
+				},
+			})
 		} catch (error) {
+			console.log(error)
 			throw error
 		}
 	}
 
 	/**
-     * @description 				- Change user's role to Mentor.
-     * @method
-     * @name 						- changeRoleToMentor
-     * @param {Object} bodyData 	- The request body containing user data.
-     * @returns {Promise<Object>} 	- A Promise that resolves to a response object.
-     */
+	 * @description 				- Change user's role to Mentor.
+	 * @method
+	 * @name 						- changeRoleToMentor
+	 * @param {Object} bodyData 	- The request body containing user data.
+	 * @returns {Promise<Object>} 	- A Promise that resolves to a response object.
+	 */
 
 	static async changeRoleToMentor(bodyData) {
 		try {
 			// Get mentee_extension data
-			const menteeDetails = await menteeQueries.getMenteeExtension(bodyData.user_id);
+			const menteeDetails = await menteeQueries.getMenteeExtension(bodyData.user_id)
 			// If no mentee present return error
-            if (!menteeDetails) {
-                return common.failureResponse({
-                    statusCode: httpStatusCode.not_found,
-                    message: 'MENTEE_EXTENSION_NOT_FOUND',
-                });
-            }
+			if (!menteeDetails) {
+				return common.failureResponse({
+					statusCode: httpStatusCode.not_found,
+					message: 'MENTEE_EXTENSION_NOT_FOUND',
+				})
+			}
 
 			// Add fetched mentee details to mentor_extension table
-            const mentorCreationData = await mentorQueries.createMentorExtension(menteeDetails);
-            if (!mentorCreationData) {
-                return common.failureResponse({
-                    message: 'MENTOR_EXTENSION_CREATION_FAILED',
-                    statusCode: httpStatusCode.bad_request,
-                    responseCode: 'CLIENT_ERROR',
-                });
-            }
+			const mentorCreationData = await mentorQueries.createMentorExtension(menteeDetails)
+			if (!mentorCreationData) {
+				return common.failureResponse({
+					message: 'MENTOR_EXTENSION_CREATION_FAILED',
+					statusCode: httpStatusCode.bad_request,
+					responseCode: 'CLIENT_ERROR',
+				})
+			}
 
 			// Delete mentee extension (user_extension table)
-            await menteeQueries.deleteMenteeExtension(bodyData.user_id);
+			await menteeQueries.deleteMenteeExtension(bodyData.user_id)
 
-            return common.successResponse({
-                statusCode: httpStatusCode.ok,
-                message: 'USER_ROLE_UPDATED',
-                result: {
-                    user_id: mentorCreationData.user_id,
-                    roles: mentorCreationData.roles
-                },
-            });
-
+			return common.successResponse({
+				statusCode: httpStatusCode.ok,
+				message: 'USER_ROLE_UPDATED',
+				result: {
+					user_id: mentorCreationData.user_id,
+					roles: mentorCreationData.roles,
+				},
+			})
 		} catch (error) {
+			console.log(error)
 			throw error
 		}
 	}
-
 }
