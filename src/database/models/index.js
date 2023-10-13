4ecb71a259a5a5124d8947e9f82a81f354bd68fd
joffinjoy <joffinjoy@gmail.com>
2023-10-13 14:41:31 +0530

    Relative Path Fix

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -8,7 +8,7 @@ const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
 // const config = require(__dirname + '/../config/config.js')[env]
 
-const config = require('../../configs/postgres.js')[env]
+const config = require('@configs/postgres.js')[env]
 
 const db = {}
 let sequelize
