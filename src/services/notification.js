7240435dccd501e1814cbf39f936248506f68848
joffinjoy <joffinjoy@gmail.com>
2023-11-07 16:13:15 +0530

    Added Org_Id to Fetch

--- src/services/notification.js
+++ src/services/notification.js
@@ -14,14 +14,17 @@ module.exports = class NotificationTemplateHelper {
 
 	static async create(bodyData, tokenInformation) {
 		try {
-			/* const template = await notificationTemplateQueries.findOne({ code: bodyData.code })
+			const template = await notificationTemplateQueries.findOne({
+				code: bodyData.code,
+				org_id: tokenInformation.organization_id,
+			})
 			if (template) {
 				return common.failureResponse({
 					message: 'NOTIFICATION_TEMPLATE_ALREADY_EXISTS',
 					statusCode: httpStatusCode.bad_request,
 					responseCode: 'CLIENT_ERROR',
 				})
-			} */
+			}
 
 			bodyData['org_id'] = tokenInformation.organization_id
 			bodyData['created_by'] = tokenInformation.id
@@ -33,6 +36,7 @@ module.exports = class NotificationTemplateHelper {
 				result: createdNotification,
 			})
 		} catch (error) {
+			console.log(error)
 			throw error
 		}
 	}
