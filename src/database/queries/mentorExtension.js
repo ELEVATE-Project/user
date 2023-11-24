06b3a2ffe2230f591f7754a70bdbc4526b5c488d
joffinjoy <joffinjoy@gmail.com>
2023-11-24 20:39:21 +0530

    MentorExtension Update Fix

--- src/database/queries/mentorExtension.js
+++ src/database/queries/mentorExtension.js
@@ -3,6 +3,7 @@ const { QueryTypes } = require('sequelize')
 const sequelize = require('sequelize')
 const Sequelize = require('@database/models/index').sequelize
 const common = require('@constants/common')
+const _ = require('lodash')
 
 module.exports = class MentorExtensionQueries {
 	static async getColumns() {
@@ -25,8 +26,7 @@ module.exports = class MentorExtensionQueries {
 			if (data.user_id) {
 				delete data['user_id']
 			}
-			const whereClause = customFilter ? customFilter : { user_id: userId }
-			console.log('WHERE CLAUSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE:', whereClause)
+			const whereClause = _.isEmpty(customFilter) ? customFilter : { user_id: userId }
 			return await MentorExtension.update(data, {
 				where: whereClause,
 				...options,
