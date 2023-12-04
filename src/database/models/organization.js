3dedd3e21fb2d578459597819392efd36bebee12
joffinjoy <joffinjoy@gmail.com>
2023-12-04 22:54:15 +0530

    Added Migrations To Make About & Description Into Text

--- src/database/models/organization.js
+++ src/database/models/organization.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
 				allowNull: false,
 			},
 			description: {
-				type: DataTypes.STRING,
+				type: DataTypes.TEXT,
 				allowNull: false,
 			},
 			status: {
