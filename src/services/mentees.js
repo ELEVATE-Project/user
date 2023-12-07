09f21afd3600ae96b6e084256d74dcb61b29659c
Nevil <nevil@tunerlabs.com>
2023-12-07 17:29:30 +0530

    updated profile update for mentee and mentor

--- src/services/mentees.js
+++ src/services/mentees.js
@@ -668,12 +668,24 @@ module.exports = class MenteesHelper {
 				raw: true,
 			})
 
-			if (updateCount === '0') {
-				return common.failureResponse({
-					statusCode: httpStatusCode.not_found,
-					message: 'MENTEE_EXTENSION_NOT_FOUND',
+			if (updateCount === 0) {
+				const fallbackUpdatedUser = await menteeQueries.getMenteeExtension(userId)
+				console.log(fallbackUpdatedUser)
+				if (!fallbackUpdatedUser) {
+					return common.failureResponse({
+						statusCode: httpStatusCode.not_found,
+						message: 'MENTEE_EXTENSION_NOT_FOUND',
+					})
+				}
+				const processDbResponse = utils.processDbResponse(fallbackUpdatedUser, validationData)
+
+				return common.successResponse({
+					statusCode: httpStatusCode.ok,
+					message: 'MENTEE_EXTENSION_UPDATED',
+					result: processDbResponse,
 				})
 			}
+
 			const processDbResponse = utils.processDbResponse(updatedUser[0], validationData)
 
 			return common.successResponse({
