6043020bc03ed668634b25c3ed6a1da4369f8951
Nevil <nevil@tunerlabs.com>
2023-12-11 17:06:47 +0530

    added migration to update the schema

--- src/database/models/userCredential.js
+++ src/database/models/userCredential.js
@@ -6,13 +6,13 @@ module.exports = (sequelize, DataTypes) => {
 			id: {
 				type: DataTypes.INTEGER,
 				allowNull: false,
-				primaryKey: true,
 				autoIncrement: true,
 			},
 			email: {
 				type: DataTypes.STRING,
 				allowNull: false,
 				unique: true,
+				primaryKey: true,
 			},
 			password: {
 				type: DataTypes.STRING,
