04a7d449e65deff9a133d4c187872d33ec044e16
sumanvpacewisdom <suman.v@pacewisdom.com>
2023-12-18 18:13:57 +0530

    userRole APIs

--- src/database/seeders/20230718130102-add_roles_to_roles_table.js
+++ src/database/seeders/20230718130102-add_roles_to_roles_table.js
@@ -3,17 +3,15 @@ module.exports = {
 		let rolesData = []
 		const defaultOrgId = queryInterface.sequelize.options.defaultOrgId
 		const roleArray = ['user', 'mentor', 'mentee', 'admin']
-		// user_type denotes the role is a system user or not 1: system user, 0: non-system user
 		roleArray.forEach(async function (role) {
 			let user_type = 0
 			if (role == 'admin') {
 				user_type = 1
 			}
-
 			let eachRow = {
 				title: role,
 				user_type: user_type,
-				visibility: 'PUBLIC', // Corrected property name here
+				visibility: 'PUBLIC',
 				organization_id: defaultOrgId,
 				updated_at: new Date(),
 				created_at: new Date(),
