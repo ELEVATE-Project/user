aa74cf16dca13d5e8bbc9211dc59d044db1c6672
joffinjoy <joffinjoy@gmail.com>
2024-01-09 18:16:33 +0530

    Made the Datafix Migration As Non-blocking As It Is Likely To Cause An Exception During Fresh Setup

--- src/database/migrations/20240109122038-fix-entity-type-data-type.js
+++ src/database/migrations/20240109122038-fix-entity-type-data-type.js
@@ -3,18 +3,26 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		await queryInterface.sequelize.query(`
-          UPDATE "entity_types"
-          SET "data_type" = 'ARRAY[STRING]'
-          WHERE "value" = 'languages';
-        `)
+		try {
+			await queryInterface.sequelize.query(`
+            UPDATE "entity_types"
+            SET "data_type" = 'ARRAY[STRING]'
+            WHERE "value" = 'languages';
+          `)
+		} catch (error) {
+			console.error('Error during migration:', error.message)
+		}
 	},
 
 	down: async (queryInterface, Sequelize) => {
-		await queryInterface.sequelize.query(`
-          UPDATE "entity_types"
-          SET "data_type" = 'STRING'
-          WHERE "value" = 'languages';
-        `)
+		try {
+			await queryInterface.sequelize.query(`
+            UPDATE "entity_types"
+            SET "data_type" = 'STRING'
+            WHERE "value" = 'languages';
+          `)
+		} catch (error) {
+			console.error('Error during migration:', error.message)
+		}
 	},
 }
