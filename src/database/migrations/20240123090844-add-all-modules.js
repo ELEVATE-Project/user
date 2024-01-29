9e137c8abd2e5598f58bfacbd20c3872060cccca
sumanvpacewisdom <suman.v@pacewisdom.com>
2024-01-29 12:57:16 +0530

    comment changes

--- src/database/migrations/20240123090844-add-all-modules.js
+++ src/database/migrations/20240123090844-add-all-modules.js
@@ -4,11 +4,6 @@
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
 		const modulesData = [
-			{ code: 'all', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
-			{ code: 'manage_session', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
-			{ code: 'mentees', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
-			{ code: 'modules', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
-			{ code: 'permissions', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
 			{ code: 'cloud_services', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
 			{ code: 'entity_type', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
 			{ code: 'entity', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
