fd7c6e95779f70bac8110e9e7e8b2d943b5de436
Nevil <nevil@tunerlabs.com>
2024-02-01 19:16:32 +0530

    removed org id

--- src/database/migrations/20240201133617-update-generic_invite.js
+++ src/database/migrations/20240201133617-update-generic_invite.js
@@ -4,16 +4,10 @@ const moment = require('moment')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		const defaultOrgId = queryInterface.sequelize.options.defaultOrgId
-		if (!defaultOrgId) {
-			throw new Error('Default org ID is undefined. Please make sure it is set in sequelize options.')
-		}
-
 		let notificationTemplateData = [
 			{
 				body: '<p>Dear {name},</p> We are delighted to inform you that you have been successfully onboarded as a {roles} for {orgName}. You can now explore {appName}. <br>We request you to register on our Mentoring Platform (if not already), to start your journey with us as a {roles}. <br><br> Click to register: {portalURL}',
 				updated_at: moment().format(),
-				organization_id: defaultOrgId,
 			},
 		]
 
