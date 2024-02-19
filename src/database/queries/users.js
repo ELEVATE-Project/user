ef197544462131afa700768900202a2314137e34
adithya_dinesh <adithya@shikshalokam.org>
2024-02-19 13:27:44 +0530

    user sorting issue fixed

--- src/database/queries/users.js
+++ src/database/queries/users.js
@@ -215,7 +215,7 @@ exports.listUsersFromView = async (roleId, organization_id, page, limit, search,
                 AND organization.status = 'ACTIVE'
             ${filterClause}
             ORDER BY
-                users.name ASC
+				LOWER(users.name) ASC
 			OFFSET
                 :offset
             LIMIT
@@ -240,19 +240,44 @@ exports.listUsersFromView = async (roleId, organization_id, page, limit, search,
 			user.email = emailEncryption.decrypt(user.email)
 			return user
 		})
-
+		// Record the start time
+		const startTime = performance.now()
 		const countQuery = ` SELECT COUNT(*) as total_count
+    				FROM m_${database.User.tableName} AS users
+    				INNER JOIN ${Organization.tableName} AS organization
+        			ON users.organization_id = organization.id
+        			AND organization.status = 'ACTIVE'
+    				${filterClause};
+				`
+		const totalCountData = await Sequelize.query(countQuery, {
+			type: QueryTypes.SELECT,
+			replacements: replacements,
+		})
+
+		// Record the end time
+		const endTime = performance.now()
+		// Calculate the elapsed time
+		const elapsedTime = endTime - startTime
+
+		// Record the start time
+		const startTime1 = performance.now()
+		const countQuery1 = ` SELECT COUNT(*) as total_count
     				FROM m_${database.User.tableName} AS users
     				LEFT JOIN ${Organization.tableName} AS organization
         			ON users.organization_id = organization.id
         			AND organization.status = 'ACTIVE'
     				${filterClause};
 				`
-
-		const totalCountData = await Sequelize.query(countQuery, {
+		const totalCountData1 = await Sequelize.query(countQuery1, {
 			type: QueryTypes.SELECT,
 			replacements: replacements,
 		})
+
+		// Record the end time
+		const endTime1 = performance.now()
+		// Calculate the elapsed time
+		const elapsedTime1 = endTime1 - startTime1
+
 		const totalCount = totalCountData.length > 0 ? Number(totalCountData[0].total_count) : 0
 
 		return { count: totalCount, data: users }
