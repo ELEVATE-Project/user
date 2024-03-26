4806aff19d7e7c40ec813fdf3441c95b8e751032
adithya_dinesh <adithya@shikshalokam.org>
2024-03-26 17:55:47 +0530

    removed interface apis

--- src/constants/common.js
+++ src/constants/common.js
@@ -89,7 +89,7 @@ module.exports = {
 	DELETED_STATUS: 'DELETED',
 	DEFAULT_ORG_VISIBILITY: 'PUBLIC',
 	ROLE_TYPE_NON_SYSTEM: 0,
-	captchaEnabledAPIs: ['/user/v1/account/login', '/interface/v1/account/login'],
+	captchaEnabledAPIs: ['/user/v1/account/login'],
 	google_recaptcha_API: {
 		HOST: 'https://www.google.com',
 		URL: '/recaptcha/api/siteverify',
