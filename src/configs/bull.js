48b5ee51a9ff46b41969477937784cb50858b052
sumanvpacewisdom <suman.v@pacewisdom.com>
2024-06-19 18:57:33 +0530

    Logs for bullmq

--- src/configs/bull.js
+++ src/configs/bull.js
@@ -1,16 +1,16 @@
 ;('use strict')
-const { Worker } = require('bullmq')
+const { Worker, Queue } = require('bullmq')
 const fs = require('fs')
 const userInviteService = require('@services/userInvite')
 const common = require('@constants/common')
 const utils = require('@generics/utils')
 const path = require('path')
-
+const { log } = require('console')
 const { elevateLog } = require('elevate-logger')
 const logger = elevateLog.init()
 
 const redisConfiguration = utils.generateRedisConfigForQueue()
-
+const logInterval = 30000
 module.exports = function () {
 	try {
 		const ProjectRootDir = path.join(__dirname, '..')
@@ -45,6 +45,26 @@ module.exports = function () {
 		worker.on('requestFail', (job, err) => {
 			logger.error(`Job with id ${job.id} Failed`)
 		})
+
+		const queue = new Queue(process.env.DEFAULT_QUEUE, { connection: redisConfiguration })
+
+		// Function to log job counts
+		const logJobCounts = async () => {
+			try {
+				const waitingCount = await queue.getWaitingCount()
+				const delayedCount = await queue.getDelayedCount()
+				const failedCount = await queue.getFailedCount()
+
+				console.log(`Waiting jobs count: ${waitingCount}`)
+				console.log(`Delayed jobs count: ${delayedCount}`)
+				console.log(`Failed jobs count: ${failedCount}`)
+			} catch (err) {
+				console.log(`Error fetching job counts: ${err.message}`)
+			}
+		}
+		logJobCounts()
+
+		setInterval(logJobCounts, logInterval)
 	} catch (err) {
 		console.error(err)
 		throw err
