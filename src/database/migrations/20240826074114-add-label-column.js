70bd11a5d3c024f9cc763833415c0edef3d8e52e
MallanagoudaB <mallanagouda@shikshalokam.org>
2024-08-26 13:56:57 +0530

    "added-map-function"

--- src/database/migrations/20240826074114-add-label-column.js
+++ src/database/migrations/20240826074114-add-label-column.js
@@ -3,32 +3,34 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		// Add the new 'label' column to the 'user-role' table
+		// Add the new 'label' column to the 'user-roles' table
 		await queryInterface.addColumn('user_roles', 'label', {
 			type: Sequelize.STRING,
-			allowNull: true, // Assuming label can be null initially
+			allowNull: false, // Assuming label can be null initially
 		})
 
-		// Fetch all rows from the 'user-role' table
+		// Fetch all rows from the 'user-roles' table
 		const userRoles = await queryInterface.sequelize.query('SELECT id, title FROM user_roles', {
 			type: queryInterface.sequelize.QueryTypes.SELECT,
 		})
-
 		// Update each row with the new label
-		for (const role of userRoles) {
+		const updatePromises = userRoles.map((role) => {
 			const label = role.title
 				.split('_')
 				.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
 				.join(' ')
 
-			await queryInterface.sequelize.query(`UPDATE user_roles SET label = :label WHERE id = :id`, {
+			return queryInterface.sequelize.query(`UPDATE user_roles SET label = :label WHERE id = :id`, {
 				replacements: { label, id: role.id },
 			})
-		}
+		})
+
+		// Wait for all update queries to complete
+		await Promise.all(updatePromises)
 	},
 
 	down: async (queryInterface, Sequelize) => {
-		// Remove the 'label' column from the 'user-role' table
+		// Remove the 'label' column from the 'user-roles' table
 		await queryInterface.removeColumn('user_roles', 'label')
 	},
 }
