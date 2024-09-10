8e86fa80e4ab6bb3c49c252ee2a741b6567e2200
Priyanka <priyanka@tunerlabs.com>
2024-09-10 15:26:14 +0530

    added content_creator role

---
+++ src/database/migrations/20240910095427-add_content_creator_and_reviewer_role.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const defaultOrgId = queryInterface.sequelize.options.defaultOrgId
+
+		await queryInterface.bulkInsert(
+			'user_roles',
+			[
+				{
+					title: 'content_creator',
+					user_type: 0,
+					visibility: 'PUBLIC',
+					organization_id: defaultOrgId,
+					updated_at: new Date(),
+					created_at: new Date(),
+				},
+				{
+					title: 'reviewer',
+					user_type: 0,
+					visibility: 'PUBLIC',
+					organization_id: defaultOrgId,
+					updated_at: new Date(),
+					created_at: new Date(),
+				},
+			],
+			{}
+		)
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('user_roles', { title: 'public' })
+	},
+}
