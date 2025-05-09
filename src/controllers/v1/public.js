e023434e85d8e6112c736d5446d72f755386ad76
nevil <nevil@tunerlabs.com>
2025-05-09 16:43:50 +0530

    fixed: an issue with host identification

--- src/controllers/v1/public.js
+++ src/controllers/v1/public.js
@@ -2,9 +2,17 @@ const publicService = require('@services/public')
 
 module.exports = class Public {
 	async branding(req) {
-		const host = req.headers.origin // e.g., 'tenant1.example.com'
-		const domain = host.split(':')[0] // in case there's a port
+		const host = req.headers.origin || '' // e.g., 'http://localhost:3000' or undefined
+		let domain = ''
 
+		if (host) {
+			try {
+				const url = new URL(host)
+				domain = url.hostname // e.g., 'localhost' or 'dev.elevate-mentoring.shikshalokam.org'
+			} catch (error) {
+				domain = host.split(':')[0] // Fallback: remove port if present
+			}
+		}
 		try {
 			return await publicService.tenantBranding(domain, req.query.org_code ? req.query.org_code : null)
 		} catch (error) {
