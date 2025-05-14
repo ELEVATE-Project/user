7488418b2753bed42af7c9d6291272a0a3af5dce
nevil <nevil@tunerlabs.com>
2025-05-14 13:03:03 +0530

    updated: org model

--- src/database/models/organization.js
+++ src/database/models/organization.js
@@ -42,7 +42,7 @@ module.exports = (sequelize, DataTypes) => {
 				type: DataTypes.STRING,
 				allowNull: false,
 			},
-			secret_code: {
+			registration_code: {
 				type: DataTypes.STRING(32),
 				allowNull: true,
 			},
