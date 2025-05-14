49f521c7339490d429a0d2b3e9a5026ad0e649e6
nevil <nevil@tunerlabs.com>
2025-05-14 12:59:16 +0530

    updated: column secret code to registration code

--- src/database/migrations/20250509114303-add-secret-code-to-organizations.js
+++ src/database/migrations/20250509114303-add-secret-code-to-organizations.js
@@ -3,24 +3,24 @@
 module.exports = {
 	async up(queryInterface, Sequelize) {
 		// Add the secretCode column
-		await queryInterface.addColumn('organizations', 'secret_code', {
+		await queryInterface.addColumn('organizations', 'registration_code', {
 			type: Sequelize.STRING(32),
 			allowNull: true,
 		})
 
 		// Add a composite unique constraint on secretCode and tenantCode
 		await queryInterface.addConstraint('organizations', {
-			fields: ['secret_code', 'tenant_code'],
+			fields: ['registration_code', 'tenant_code'],
 			type: 'unique',
-			name: 'unique_secret_code_tenant_code',
+			name: 'unique_registration_code_tenant_code',
 		})
 	},
 
 	async down(queryInterface, Sequelize) {
 		// Remove the composite unique constraint
-		await queryInterface.removeConstraint('organizations', 'unique_secret_code_tenant_code')
+		await queryInterface.removeConstraint('organizations', 'unique_registration_code_tenant_code')
 
 		// Remove the secretCode column
-		await queryInterface.removeColumn('organizations', 'secret_code')
+		await queryInterface.removeColumn('organizations', 'registration_code')
 	},
 }
