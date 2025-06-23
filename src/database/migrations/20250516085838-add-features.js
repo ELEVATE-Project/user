57cdadf86964ab0453edaac5cd4af8698be9a08b
priyanka-TL <priyankapratheepan@gmail.com>
2025-06-23 09:22:23 +0530

    added mitra as a feature

--- src/database/migrations/20250516085838-add-features.js
+++ src/database/migrations/20250516085838-add-features.js
@@ -47,6 +47,13 @@ module.exports = {
 				created_at: moment().format(),
 				updated_at: moment().format(),
 			},
+			{
+				code: 'mitra',
+				label: 'MITRA',
+				description: 'mitra capability',
+				created_at: moment().format(),
+				updated_at: moment().format(),
+			},
 		]
 
 		await queryInterface.bulkInsert('features', masterFeatureData, {})
