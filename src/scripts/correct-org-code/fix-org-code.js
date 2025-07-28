42126ecf6ca22007672780ba36fafe4dbdbf974c
adithya_dinesh <adithya@shikshalokam.org>
2025-07-28 18:51:19 +0530

    final

--- src/scripts/correct-org-code/fix-org-code.js
+++ src/scripts/correct-org-code/fix-org-code.js
@@ -4,6 +4,8 @@ require('dotenv').config({ path: '../../.env' })
 // Environment setup
 const nodeEnv = process.env.NODE_ENV || 'development'
 
+let fk_retainer = []
+let table, fk_name, fkey, refTable, refKey
 let databaseUrl
 
 switch (nodeEnv) {
@@ -33,182 +35,136 @@ const sequelize = new Sequelize(databaseUrl, {
 
 		const DISABLE_FK_QUERY = 'SET CONSTRAINTS ALL DEFERRED' // temporarily remove constraint checks till the transaction is completed
 
-		const ORG_UPDATE_QUERY = `UPDATE organizations SET code = REGEXP_REPLACE(code, '\s+', '', 'g') WHERE code ~ '\s+';`
 		const ORG_FETCH_QUERY = `SELECT id, name, code FROM organizations WHERE code ~ '\s+';`
+		const disableFK = (table, fk_name) => `ALTER TABLE ${table} DROP CONSTRAINT IF EXISTS ${fk_name};`
+		const enableFK = (table, fk_name, fkey, refTable, refKey) =>
+			`ALTER TABLE ${table} ADD CONSTRAINT ${fk_name} FOREIGN KEY ${fkey} REFERENCES ${refTable} ${refKey} ON UPDATE NO ACTION ON DELETE CASCADE;`
+		const updateQuery = (table, key) =>
+			`UPDATE ${table} SET ${key} = REGEXP_REPLACE(${key}, '\\s+', '', 'g') WHERE ${key} ~ '\\s+';`
 
+		// Execute the query with replacements
+		const fetchOrg = await sequelize.query(ORG_FETCH_QUERY, {
+			type: Sequelize.QueryTypes.SELECT,
+			raw: true,
+			transaction,
+		})
+
+		console.log(fetchOrg)
 		// Test database connection
 		await sequelize.authenticate()
 		console.log('Database connection established successfully.')
+
+		table = 'organization_registration_codes'
+		fk_name = 'fk_organization_code_tenant_code_in_org_reg_code'
+		fkey = '(organization_code, tenant_code)'
+		refTable = 'organizations'
+		refKey = '(code, tenant_code)'
+		fk_retainer.push(enableFK(table, fk_name, fkey, refTable, refKey))
+		await sequelize.query(disableFK(table, fk_name), { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
 		await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
+
+		table = 'user_organizations'
+		fk_name = 'fk_user_organizations_organizations'
+		fkey = '(organization_code, tenant_code)'
+		refTable = 'organizations'
+		refKey = '(code, tenant_code)'
+		await sequelize.query(disableFK(table, fk_name), { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
+		fk_retainer.push(enableFK(table, fk_name, fkey, refTable, refKey))
+
+		table = 'organization_user_invites'
+		fk_name = 'fk_org_user_invites_org_code'
+		fkey = '(organization_code, tenant_code)'
+		refTable = 'organizations'
+		refKey = '(code, tenant_code)'
+		await sequelize.query(disableFK(table, fk_name), { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
+		await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
+		fk_retainer.push(enableFK(table, fk_name, fkey, refTable, refKey))
+
+		table = 'organization_user_invites'
+		fk_name = 'fk_org_user_invites_organization_id'
+		fkey = '(organization_code, tenant_code)'
+		refTable = 'organizations'
+		refKey = '(code, tenant_code)'
+		await sequelize.query(disableFK(table, fk_name), { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
+		await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
+		fk_retainer.push(enableFK(table, fk_name, fkey, refTable, refKey))
+
+		table = 'user_organization_roles'
+		fk_name = 'fk_user_org_roles_user_organizations'
+		fkey = '(user_id, organization_code, tenant_code)'
+		refTable = 'user_organizations'
+		refKey = '(user_id, organization_code, tenant_code)'
+		await sequelize.query(disableFK(table, fk_name), { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
+		await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
+		fk_retainer.push(enableFK(table, fk_name, fkey, refTable, refKey))
+
+		let updateTable = 'organizations'
+		let key = 'code'
 		// Execute the query with replacements
-		const updateOrgs = await sequelize.query(ORG_UPDATE_QUERY, {
+		const updateOrgs = await sequelize.query(updateQuery(updateTable, key), {
 			type: Sequelize.QueryTypes.UPDATE,
 			raw: true,
 			transaction,
 		})
+		console.log('------------>>', updateOrgs)
+		updateTable = 'organization_registration_codes'
+		key = 'organization_code'
+		// Execute the query with replacements
+		await sequelize.query(updateQuery(updateTable, key), {
+			type: Sequelize.QueryTypes.UPDATE,
+			raw: true,
+			transaction,
+		})
+
+		updateTable = 'organization_user_invites'
+		key = 'organization_code'
+		// Execute the query with replacements
+		await sequelize.query(updateQuery(updateTable, key), {
+			type: Sequelize.QueryTypes.UPDATE,
+			raw: true,
+			transaction,
+		})
+
+		updateTable = 'user_organizations'
+		key = 'organization_code'
+		// Execute the query with replacements
+		await sequelize.query(updateQuery(updateTable, key), {
+			type: Sequelize.QueryTypes.UPDATE,
+			raw: true,
+			transaction,
+		})
+		updateTable = 'user_organization_roles'
+		key = 'organization_code'
+		// Execute the query with replacements
+		await sequelize.query(updateQuery(updateTable, key), {
+			type: Sequelize.QueryTypes.UPDATE,
+			raw: true,
+			transaction,
+		})
+
 		// Execute the query with replacements
 		const fetchOrgs = await sequelize.query(ORG_FETCH_QUERY, {
-			type: Sequelize.QueryTypes.UPDATE,
+			type: Sequelize.QueryTypes.SELECT,
 			raw: true,
 			transaction,
 		})
 
 		console.log(fetchOrgs)
-		if (transaction) await transaction.rollback()
-		/*
-        const orgsToRemove = fetchOrgs
-            .filter((orgs) => orgs.code !== process.env.DEFAULT_ORGANISATION_CODE)
-            .map((orgs) => orgs.id)
-        const orgsCodesToRemove = fetchOrgs
-            .map((orgs) => orgs.code)
-            .filter((code) => code !== process.env.DEFAULT_ORGANISATION_CODE)
-        console.info('Organizations to remove: ', fetchOrgs)
+		let fk_retainerPromise = []
+		for (let i = 0; i < fk_retainer.length; i++) {
+			fk_retainerPromise.push(
+				sequelize.query(fk_retainer[i], {
+					type: Sequelize.QueryTypes.UPDATE,
+					raw: true,
+					transaction,
+				})
+			)
+		}
 
-        // Fetch system admin and org admin roles
-        const allowedRoles = ['admin', 'org_admin']
-        const ROLES_FETCH_QUERY =
-            'SELECT id FROM user_roles WHERE tenant_code IN (:allowedTenants) AND title IN (:allowedRoles)'
-        const fetchRoles = await sequelize.query(ROLES_FETCH_QUERY, {
-            replacements: { allowedTenants: allowed_tenants, allowedRoles },
-            type: Sequelize.QueryTypes.SELECT,
-            raw: true,
-            transaction
-        })
+		await Promise.all(fk_retainerPromise)
 
-        const systemRoles = fetchRoles.map((role) => role.id)
-        console.log('System Role ids: ', systemRoles)
+		if (transaction) await transaction.commit()
 
-        const FETCH_USERID_QUERY = 'SELECT user_id FROM user_organization_roles WHERE role_id IN (:systemRoles)'
-        const fetchUserIds = await sequelize.query(FETCH_USERID_QUERY, {
-            replacements: { orgsCodesToRemove, systemRoles, allowedTenants: allowed_tenants },
-            type: Sequelize.QueryTypes.SELECT,
-            raw: true,
-            transaction
-        })
-
-        const adminRoles = fetchUserIds.map((user) => user.user_id)
-
-        const REFETCH_USERID_QUERY =
-            'SELECT user_id FROM user_organization_roles WHERE user_id NOT IN (:adminRoles) OR organization_code IN (:orgsCodesToRemove)'
-        const reFetchUserIds = await sequelize.query(REFETCH_USERID_QUERY, {
-            replacements: { adminRoles, orgsCodesToRemove },
-            type: Sequelize.QueryTypes.SELECT,
-            raw: true,
-            transaction
-        })
-
-        const userIdsToDelete = reFetchUserIds.map((user) => user.user_id)
-        console.log('User ids to delete: ', userIdsToDelete)
-
-        // Delete user org roles
-        const DELETE_USER_ORG_ROLES_QUERY = 'DELETE FROM user_organization_roles WHERE user_id IN (:userIdsToDelete)'
-        await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
-        const deleteOrgUserRoles = await sequelize.query(DELETE_USER_ORG_ROLES_QUERY, {
-            replacements: { userIdsToDelete },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Delete user organizations
-        const DELETE_USER_ORGS_QUERY = 'DELETE FROM user_organizations WHERE user_id IN (:userIdsToDelete)'
-        await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
-        const deleteOrgUsers = await sequelize.query(DELETE_USER_ORGS_QUERY, {
-            replacements: { userIdsToDelete },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Delete users
-        const DELETE_USERS_QUERY = 'DELETE FROM users WHERE id IN (:userIdsToDelete)'
-        await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
-        const deleteUsers = await sequelize.query(DELETE_USERS_QUERY, {
-            replacements: { userIdsToDelete },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Truncate uploads
-        const TRUNCATE_UPLOADS_QUERY = 'TRUNCATE TABLE file_uploads RESTART IDENTITY'
-        const truncateUploads = await sequelize.query(TRUNCATE_UPLOADS_QUERY, {
-            type: Sequelize.QueryTypes.RAW,
-            raw: true,
-            transaction
-        })
-
-        // Truncate invites
-        const TRUNCATE_INVITES_QUERY = 'TRUNCATE TABLE organization_user_invites RESTART IDENTITY'
-        const truncateInvites = await sequelize.query(TRUNCATE_INVITES_QUERY, {
-            type: Sequelize.QueryTypes.RAW,
-            raw: true,
-            transaction
-        })
-
-        // Delete organization features
-        const DELETE_ORG_FEATURES_QUERY =
-            'DELETE FROM organization_features WHERE organization_code IN (:orgsCodesToRemove)'
-        await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
-        const deleteOrganizationFeatures = await sequelize.query(DELETE_ORG_FEATURES_QUERY, {
-            replacements: { orgsCodesToRemove },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Delete organization domains
-        const DELETE_ORG_DOMAINS_QUERY =
-            'DELETE FROM organization_email_domains WHERE organization_id IN (:orgsToRemove)'
-        await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
-        const deleteOrganizationDomains = await sequelize.query(DELETE_ORG_DOMAINS_QUERY, {
-            replacements: { orgsToRemove },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Delete organization role requests
-        const DELETE_ORG_ROLE_REQUEST_QUERY =
-            'DELETE FROM organization_role_requests WHERE organization_id IN (:orgsToRemove)'
-        const deleteOrganizatioRoleRequest = await sequelize.query(DELETE_ORG_ROLE_REQUEST_QUERY, {
-            replacements: { orgsToRemove },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Delete organizations
-        const DELETE_ORGS_QUERY = 'DELETE FROM organizations WHERE id IN (:orgsToRemove)'
-        await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
-        const deleteOrganizations = await sequelize.query(DELETE_ORGS_QUERY, {
-            replacements: { orgsToRemove },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Delete tenant domains
-        const DELETE_TENANT_DOMAINS_QUERY = 'DELETE FROM tenant_domains WHERE tenant_code NOT IN (:allowed_tenants)'
-        const deleteTenantDomains = await sequelize.query(DELETE_TENANT_DOMAINS_QUERY, {
-            replacements: { allowed_tenants },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Delete tenants
-        const DELETE_TENANTS_QUERY = 'DELETE FROM tenants WHERE code NOT IN (:allowed_tenants)'
-        await sequelize.query(DISABLE_FK_QUERY, { type: Sequelize.QueryTypes.RAW, raw: true, transaction })
-        const deleteTenants = await sequelize.query(DELETE_TENANTS_QUERY, {
-            replacements: { allowed_tenants },
-            type: Sequelize.QueryTypes.DELETE,
-            raw: true,
-            transaction
-        })
-
-        // Commit the transaction
-        await transaction.commit(); */
 		console.log('Transaction committed successfully.')
 	} catch (error) {
 		// Rollback transaction on error
