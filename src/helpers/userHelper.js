b6b80aae06f4aa0a6be6653b34ff4e4018d23f50
Nevil Mathew <nevil@tunerlabs.com>
2025-09-15 18:29:08 +0530

    feat: implement cache clearing for deleted user profiles

--- src/helpers/userHelper.js
+++ src/helpers/userHelper.js
@@ -1,11 +1,11 @@
 const common = require('@constants/common')
+const cacheClient = require('@generics/cacheHelper')
 const utils = require('@generics/utils')
 const userQueries = require('@database/queries/users')
 const userOrganizationQueries = require('@database/queries/userOrganization')
 const userOrganizationRoleQueries = require('@database/queries/userOrganizationRole')
 const userSessionsService = require('@services/user-sessions')
 const Sequelize = require('@database/models/index').sequelize
-const REDIS_USER_PREFIX = common.redisUserPrefix
 const DELETED_STATUS = common.DELETED_STATUS
 const userSessionsQueries = require('@database/queries/user-sessions')
 
@@ -77,7 +77,6 @@ const userHelper = {
 					{ user_id: user.id, tenant_code: user.tenant_code },
 					{ transaction }
 				)
-				await utils.redisDel([`${REDIS_USER_PREFIX}${user.tenant_code}_${userId}`])
 
 				const userSessionData = await userSessionsService.findUserSession(
 					{
@@ -91,6 +90,20 @@ const userHelper = {
 				)
 				const userSessionIds = userSessionData.map(({ id }) => id)
 				await userSessionsService.removeUserSessions(userSessionIds)
+
+				// Clear cache entries for this user
+				try {
+					const ns = common.CACHE_CONFIG.namespaces.profile.name
+					const fullKey = await cacheClient.versionedKey({
+						tenantCode: user.tenant_code,
+						orgId: user.organization_code,
+						ns,
+						id: userId,
+					})
+					await cacheClient.del(fullKey)
+				} catch (err) {
+					console.error('Failed to delete user cache', err)
+				}
 			},
 			transactionOptions
 		)
