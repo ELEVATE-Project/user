dfab4ace4a8b1110604ea00fcdf9b29585a57957
Nevil Mathew <nevil@tunerlabs.com>
2026-07-16 18:23:59 +0530

    refactor: update CircleCI config and environment variables; remove unused test commands and sensitive data

--- src/validators/v1/public.js
+++ src/validators/v1/public.js
@@ -3,11 +3,7 @@
  * Description : Validations of public controller
  */
 module.exports = {
-	branding: (req) => {},
-
 	checkUsername: (req) => {
 		req.checkQuery('username').trim().notEmpty().withMessage('username field is empty')
 	},
-
-	userInvites: (req) => {},
 }
